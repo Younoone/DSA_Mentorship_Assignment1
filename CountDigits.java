b5714417bfe7c2e2ead25dbbb861e830d0cb1eee
Khushi <107745036+Younoone@users.noreply.github.com>
2023-09-28 23:43:28 +0530

    Create CountDigits.java

---
+++ CountDigits.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class CountDigits
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter your no.");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int count = 0;
+        while(n>0)
+        {
+            n = n / 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
